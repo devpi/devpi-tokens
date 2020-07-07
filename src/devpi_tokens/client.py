9099f514726012170353be15aa334a3bbcacb155
Florian Schulze <mail@florian-schulze.net>
2020-07-07 13:24:29 +0200

    Add token-login command.

--- src/devpi_tokens/client.py
+++ src/devpi_tokens/client.py
@@ -1,4 +1,9 @@
+from getpass import getpass
 from pluggy import HookimplMarker
+import py
+import pymacaroons
+import sys
+import traceback
 
 
 client_hookimpl = HookimplMarker("devpiclient")
@@ -17,7 +22,40 @@ def create_token(hub, args):
     hub.line(token)
 
 
+def token_login_arguments(parser):
+    """ Login using a token.
+    """
+    parser.add_argument(
+        "-f", "--file", action="store", default=None,
+        help="file containing the token, use - to read from standard input")
+
+
+def token_login(hub, args):
+    if args.file:
+        if args.file == "-":
+            token = sys.stdin.readline().strip()
+        else:
+            path = py.path.local(args.file)
+            if not path.exists():
+                hub.fatal("The file for the token doesn't exist.")
+            with path.open(encoding="ascii") as f:
+                token = f.readline().strip()
+    else:
+        token = getpass("token: ")
+    try:
+        macaroon = pymacaroons.Macaroon.deserialize(token)
+        (token_user, token_id) = macaroon.identifier.decode("ascii").rsplit('-', 1)
+    except Exception as e:
+        hub.fatal("Invalid token: %s" % "".join(traceback.format_exception_only(e.__class__, e)))
+    hub.current.set_auth(token_user, token)
+    msg = "logged in %r" % token_user
+    if hub.current.index:
+        msg = "%s at %r" % (msg, hub.current.index)
+    hub.info(msg)
+
+
 @client_hookimpl
 def devpiclient_subcommands():
     return [
-        (create_token_arguments, "create-token", "devpi_tokens.client:create_token")]
+        (create_token_arguments, "create-token", "devpi_tokens.client:create_token"),
+        (token_login_arguments, "token-login", "devpi_tokens.client:token_login")]
