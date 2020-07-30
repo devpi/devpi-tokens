902628d1a8d45805169fe6031cef46ec2747f612
Florian Schulze <mail@florian-schulze.net>
2020-07-30 10:11:03 +0200

    Refactor into several functions for reuse.

--- src/devpi_tokens/client.py
+++ src/devpi_tokens/client.py
@@ -9,6 +9,46 @@ import traceback
 client_hookimpl = HookimplMarker("devpiclient")
 
 
+def add_token_args(parser):
+    parser.add_argument(
+        "-f", "--file", action="store", default=None,
+        help="file containing the token, use - to read from standard input")
+    parser.add_argument(
+        "--token", action="store", default=None,
+        help="the token as a parameter, not recommended as it can easily "
+             "appear in log files etc")
+
+
+def get_token_from_args(hub, args, use_getpass=True):
+    if args.file:
+        if args.file == "-":
+            return sys.stdin.readline().strip()
+        else:
+            path = py.path.local(args.file)
+            if not path.exists():
+                hub.fatal("The file for the token doesn't exist.")
+            with path.open(encoding="ascii") as f:
+                return f.readline().strip()
+    elif args.token:
+        return args.token
+    elif use_getpass:
+        return getpass("token: ")
+
+
+def get_token_macaroon(hub, token):
+    try:
+        return pymacaroons.Macaroon.deserialize(token)
+    except Exception as e:
+        hub.fatal("Invalid token: %s" % "".join(traceback.format_exception_only(e.__class__, e)))
+
+
+def get_macaroon_user_id(hub, macaroon):
+    try:
+        return macaroon.identifier.decode("ascii").rsplit('-', 1)
+    except Exception as e:
+        hub.fatal("Invalid token: %s" % "".join(traceback.format_exception_only(e.__class__, e)))
+
+
 def create_token_arguments(parser):
     """ Create a token for current user.
     """
@@ -25,34 +65,13 @@ def create_token(hub, args):
 def token_login_arguments(parser):
     """ Login using a token.
     """
-    parser.add_argument(
-        "-f", "--file", action="store", default=None,
-        help="file containing the token, use - to read from standard input")
-    parser.add_argument(
-        "--token", action="store", default=None,
-        help="the token as a parameter, not recommended as it can easily "
-             "appear in log files etc")
+    add_token_args(parser)
 
 
 def token_login(hub, args):
-    if args.file:
-        if args.file == "-":
-            token = sys.stdin.readline().strip()
-        else:
-            path = py.path.local(args.file)
-            if not path.exists():
-                hub.fatal("The file for the token doesn't exist.")
-            with path.open(encoding="ascii") as f:
-                token = f.readline().strip()
-    elif args.token:
-        token = args.token
-    else:
-        token = getpass("token: ")
-    try:
-        macaroon = pymacaroons.Macaroon.deserialize(token)
-        (token_user, token_id) = macaroon.identifier.decode("ascii").rsplit('-', 1)
-    except Exception as e:
-        hub.fatal("Invalid token: %s" % "".join(traceback.format_exception_only(e.__class__, e)))
+    token = get_token_from_args(hub, args)
+    macaroon = get_token_macaroon(hub, token)
+    (token_user, token_id) = get_macaroon_user_id(hub, macaroon)
     hub.current.set_auth(token_user, token)
     msg = "logged in %r" % token_user
     if hub.current.index:
